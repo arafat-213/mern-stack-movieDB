e4185b86ed1c199850c26c2276bf14e50a82eeb1
arafat-213 <tai.arafat.at@gmail.com>
2020-05-01 17:14:53 +0530

    added landing page template

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = '/api/users'
+
+export const API_KEY = '3610cb45f72d9b2b29772bddf4af1493'
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+
+export const IMAGE_URL = 'https://image.tmdb.org/t/p'
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,54 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
-
+import React, { useEffect } from 'react'
+import { FaCode } from 'react-icons/fa'
+import { API_KEY, API_URL } from '../../Config'
+import MainImage from './Sections/MainImage'
+import { Typography, Row } from 'antd'
+const { Title } = Typography
 function LandingPage() {
-    return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+	useEffect(() => {
+		fetch(
+			`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+		)
+			.then(response => response.json())
+			.then(response => {
+				console.log(response)
+			})
+	}, [])
+	return (
+		<div style={{ width: '100%', margin: 0 }}>
+			{/* Movie main image */}
+			<MainImage />
+			<div>
+				<div
+					style={{
+						position: 'absolute',
+						maxWidth: '500px',
+						bottom: '2rem',
+						marginLeft: '2rem'
+					}}>
+					<Title style={{ color: 'white' }} level={2}>
+						Title
+					</Title>
+					<p style={{ color: 'white', fontSize: '1rem' }}>text</p>
+				</div>
+			</div>
+
+			{/* Body */}
+			<div style={{ width: '85%', margin: '1rem auto' }}>
+				<Title level={2}>Movies by latest</Title>
+				<hr />
+
+				{/* Grid cards */}
+				<Row gutter={[16, 16]}></Row>
+
+				{/* Load more button */}
+				<br />
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<button onClick> Load More </button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default LandingPage
